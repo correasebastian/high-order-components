a4ab29a5f5a1d3b0d0247c94ba71530b5c41b061
sebastian correa <scorrea1@wsgc.com>
2018-02-10 22:07:39 -0500

    Flatten a Prop using Recompose

--- src/App.js
+++ src/App.js
@@ -1,22 +1,35 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import {   withProps } from "recompose";
+import { compose, flattenProp, withProps } from 'recompose' ;
+const { connect } = ReactRedux();
 
-const HomeLink = withProps(({ query }) => ({ href: '#/?query=' + query }))('a');
-const ProductsLink = withProps({ href: '#/products' })('a');
-const CheckoutLink = withProps({ href: '#/checkout' })('a');
+const mapStateToProps = (state) => ({ user: state.user });
+
+const enhance = compose(
+  connect(mapStateToProps),
+  flattenProp('user')
+);
+
+const User = enhance(({ name, status }) =>
+  <div className="User"> { name } - { status } </div>
+);
 
 const App = () =>
   <div className="App">
-    <header>
-      <HomeLink query="logo">Logo</HomeLink>
-    </header>
-    <nav>
-      <HomeLink>Home</HomeLink>
-      <ProductsLink>Products</ProductsLink>
-      <CheckoutLink>Checkout</CheckoutLink>
-    </nav>
+    <User />
   </div>;
 
+
+
+// Mock Implemenation of ReactRedux connect
+function ReactRedux() {
+  const state = {
+    user: { name: 'Tim', status: 'active' }
+  };
+
+  return {
+    connect: (map) => withProps(map(state))
+  }
+}
 export default App;
