3354fa0a62bbcc718d37c5c9105bfd58227888ae
sebastian correa <scorrea1@wsgc.com>
2018-02-10 19:45:46 -0500

    withState from recompose

--- src/App.js
+++ src/App.js
@@ -1,33 +1,51 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { setPropTypes, setDisplayName, compose } from "recompose";
+import { withState } from "recompose";
 import PropTypes from 'prop-types'
 
 
-const enhancer = compose(
-  setDisplayName('UserX'),
-  setPropTypes({
-    name:PropTypes.string.isRequired
-  })
-)
-
-
-
-
-const User = ({name}) =>(
-  <div className='User'>
-    {name}
+const StatusList = () => (
+  <div className='StatusList'>
+    <div>pending</div>
+    <div>inactive</div>
+    <div>active</div>
   </div>
+);
+
+
+const Status = withState('listShown', 'setListVisible', false)(
+  ({ status,listShown, setListVisible  }) => (
+    <div onClick={()=> setListVisible((x)=> !x)}>
+      {status}
+      {listShown && <StatusList />}
+    </div>
+  )
+);
+
+const Tooltip = withState('tooltipShown', 'setTooltipVisible', false)(
+  ({ text, children, tooltipShown, setTooltipVisible }) => (
+    <span>
+      { tooltipShown && <div className='Tooltip'> {text} </div>}
+      <span onMouseEnter={()=>setTooltipVisible(true)} onMouseLeave={()=>setTooltipVisible(false)}> 
+        {children} 
+      </span>
+    </span>
+  )
 )
+ 
 
 
-const User2 = enhancer(User)
+const User = ({ name, status }) =>
+  <div className="User">
+    <Tooltip text="Cool Dude!">{ name }</Tooltip>—
+    <Status status={ status }  />
+  </div>;
+
 
 const App = () =>(
   <div>
-    <User name='Tim'/>
-    <User2 name='Joe'/>
+    <User name='Tim' status="active"/>
   </div>
 )
 
