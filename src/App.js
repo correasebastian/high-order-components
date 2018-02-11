c008a7f4bb179cc4e74cf8c4cc7be8200d87a5ab
sebastian correa <scorrea1@wsgc.com>
2018-02-10 22:41:37 -0500

    Render Components Based on Predicates in Recompose

--- src/App.js
+++ src/App.js
@@ -1,79 +1,52 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { compose, flattenProp, withProps, lifecycle, branch, renderComponent } from 'recompose' ;
+import { compose, renderComponent, branch, renderNothing } from 'recompose' ;
 
 
 const User = ({ name, status }) =>
   <div className="User">{ name }—{ status }</div>;
 
-const withUserData = lifecycle({
-  componentDidMount() {
-    fetchData().then(
-      (users) => this.setState({ users }),
-      (error) => this.setState({ error })
-    );
-  }
-});
+const userIsNotActive = ({ status }) => status !== 'active';
+const hideIfNotActive = branch(userIsNotActive, renderNothing);
 
-const UNAUTHENTICATED = 401;
-const UNAUTHORIZED = 403;
-const errorMsgs = {
-  [UNAUTHENTICATED]: 'Not Authenticated!',
-  [UNAUTHORIZED]: 'Not Authorized!',
-};
-
-const AuthError = ({ error }) =>
-    <div className="Error">{ errorMsgs[error.statusCode] }</div>;
-
-const NoUsersMessage = () =>
-  <div>There are no users to display</div>;
-
-const hasErrorCode = ({ error }) => error && error.statusCode;
-const hasNoUsers = ({ users }) => users && users.length === 0;
-
-const nonOptimalStates = (states) =>
-  compose(...states.map(state =>
-    branch(state.when, renderComponent(state.render))));
-
-const enhance = compose(
-  withUserData,
-  nonOptimalStates([
-    { when: hasErrorCode, render: AuthError },
-    { when: hasNoUsers, render: NoUsersMessage }
-  ])
-);
-
-const UserList = enhance(({ users, error }) =>
-  <div className="UserList">
-    { users && users.map((user, index) => <User key={index} {...user} />) }
+const FeaturedUser = hideIfNotActive(({ name, status }) =>
+  <div>
+    <h3>Today's Featured User</h3>
+    <User name={ name } status={ status } />
+    <hr />
   </div>
 );
 
-const App = () =>
-  <div className="App">
-    <UserList />
+const UserList = ({ users }) =>
+  <div className="UserList">
+    <h3>All Users</h3>
+    { users && users.map((user, idx) => <User key={idx} {...user} />) }
   </div>;
 
-
-// Mock Service
-const noUsers = [];
 const users = [
   { name: "Tim", status: "active" },
   { name: "Bob", status: "active" },
   { name: "Joe", status: "inactive" },
   { name: "Jim", status: "pending" },
 ];
-function fetchData() {
-  return new Promise((resolve, reject) => {
-    setTimeout(() => {
-      // reject({ statusCode: UNAUTHENTICATED });
-      // reject({ statusCode: UNAUTHORIZED })
-      // resolve(noUsers);
-      resolve(users);
-    }, 100);
-  });
-}
 
+const featured = users[getRandomInt(0, 3)];
+
+const App = () =>
+  <div className="App">
+    <h2>User Management</h2>
+    <hr />
+    <FeaturedUser name={ featured.name } status={ featured.status } />
+    <UserList users={ users } />
+  </div>;
+
+
+
+function getRandomInt(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min)) + min;
+}
 
 export default App;
