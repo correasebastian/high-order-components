0dc91feacb65d072bdebf6b039563b362406607e
sebastian correa <scorrea1@wsgc.com>
2018-02-11 11:32:03 -0500

    Not optimize Re render

--- src/App.js
+++ src/App.js
@@ -1,65 +1,66 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
-import { compose, renderComponent, branch, renderNothing, withProps, componentFromProp, withPropsOnChange, withState } from 'recompose' ;
+/*
+TITLE:
+Optimize Component Rendering using Recompose
 
+DESCRIPTION:
+Learn how and when to use the ‘pure’, ‘onlyUpdateForKeys’,
+‘onlyUpdateForPropTypes’, and ‘shouldUpdate’ higher order components.
+Each one provides a certain granularity of control over prevent
+unnecessary renders. Learn how they build upon each other to provide
+different options.
+*/
+import React from 'react';
+import { compose, withState, withHandlers, pure, onlyUpdateForKeys,
+        setPropTypes, onlyUpdateForPropTypes, shouldUpdate } from  'recompose'
 
-const lazyResult = withPropsOnChange(
-  ['depth'],
-  ({ depth }) => ({
-    result: fibonacci(depth)
+import './App.css'
+
+const Cell = ({ data, onChange, width }) =>
+  <div className="Cell" style={{ width: `${width}%`, borderColor: randomColor() }} >
+    <textarea type="text" value={ data } onChange={ onChange } />
+  </div>
+;
+const Spreadsheet = ({ rows, cols, cellsData, onCellChange }) =>
+  <div className = "Spreadsheet" > {
+    range(rows)
+    .map((row, i) =>
+      range(cols)
+      .map((col, j) => `${i}-${j}`)
+      .map(id =>
+        <Cell key={id} id={id} data={cellsData[id] || ''} 
+          onChange={ e => onCellChange(id, e.target.value) } width = { 100 / cols } 
+        />
+      )
+    )
+  }
+  </div>;
+
+const enhance = compose(
+  withState('cellsData', 'setCells', {}),
+  withHandlers({
+    setCellState: ({ cellsData, setCells }) => (id, val) =>
+      setCells({
+        ...cellsData,
+        [id]: val
+      })
   })
 );
 
-const Fibonacci = lazyResult(({ result, color, size }) =>
-  <div style={{ color, fontSize: size }}>
-    Fibonacci Result:<br/>{ result }
-  </div>
-);
-
-const withAppState = compose(
-  withState('depth', 'setDepth', 1400),
-  withState('color', 'setColor', 'red'),
-  withState('size', 'setSize', 14)
-);
-
-const App = withAppState(({ depth, color, size, setDepth, setColor, setSize }) =>
+const App = enhance(({ cellsData, setCellState }) =>
   <div className="App">
-    <Fibonacci { ...{ depth, color, size} } />
-    <br/>
-    <div>
-      <span>Depth: { depth } </span>
-      <button onClick={ () => setDepth(x => x + 1) }>+</button>
-      <button onClick={ () => setDepth(x => x - 1) }>-</button>
-    </div>
-    <div>
-      <span>Size: { size } </span>
-      <button onClick={ () => setSize(x => x + 1) }>+</button>
-      <button onClick={ () => setSize(x => x - 1) }>-</button>
-    </div>
-    <div>
-      <span>Color: </span>
-      <button onClick={ () => setColor('blue') }>blue</button>
-      <button onClick={ () => setColor('green') }>green</button>
-      <button onClick={ () => setColor('red') }>red</button>
-    </div>
-    <br/>
+    <Spreadsheet
+      {...{ rows: 3, cols: 3, cellsData, onCellChange: setCellState }}
+    />
   </div>
 );
 
 
-let count = 1;
-
-function fibonacci(num, memo) {
-  if (!memo) {
-    document.getElementById('log').textContent = `Computed: ${++count || 1}`;
-  }
-
-  memo = memo || {};
-
-  if (memo[num]) return memo[num];
-  if (num <= 1) return 1;
-
-  return memo[num] = fibonacci(num - 1, memo) + fibonacci(num - 2, memo);
+function range(num) {
+  return Array.from(Array(num).keys())
 }
-export default App;
+
+function randomColor() {
+  return '#'+Math.floor(Math.random()*16777215).toString(16);
+}
+
+export default App
