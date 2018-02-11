3dc83c5e2efa885169fc354e30035aabea74e7be
sebastian correa <scorrea1@wsgc.com>
2018-02-10 22:03:06 -0500

    Lock Props by using the Recompose withProps Higher Order Component

--- src/App.js
+++ src/App.js
@@ -1,40 +1,22 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import {  mapProps } from "recompose";
+import {   withProps } from "recompose";
 
-const User = ({ name, status }) =>
-  <div className="User">{ name }—{ status }</div>;
-
-const UserList = ({ users, status }) =>
-  <div className="UserList">
-    <h3>{ status } users</h3>
-    { users && users.map((user, index) => <User key={index}  {...user} />) }
-  </div>;
-
-const users = [
-  { name: "Tim", status: 'active' },
-  { name: "Bob", status: 'active' },
-  { name: "Joe", status: 'pending' },
-  { name: "Jim", status: 'inactive' },
-];
-
-const filterByStatus = (status) => mapProps(
-  ({ users }) => ({
-    status,
-    users: users.filter(u => u.status === status)
-  })
-);
-
-const ActiveUsers = filterByStatus('active')(UserList);
-const InactiveUsers = filterByStatus('inactive')(UserList);
-const PendingUsers = filterByStatus('pending')(UserList);
+const HomeLink = withProps(({ query }) => ({ href: '#/?query=' + query }))('a');
+const ProductsLink = withProps({ href: '#/products' })('a');
+const CheckoutLink = withProps({ href: '#/checkout' })('a');
 
 const App = () =>
   <div className="App">
-    <ActiveUsers users={ users } />
-    <InactiveUsers users={ users } />
-    <PendingUsers users={ users } />
+    <header>
+      <HomeLink query="logo">Logo</HomeLink>
+    </header>
+    <nav>
+      <HomeLink>Home</HomeLink>
+      <ProductsLink>Products</ProductsLink>
+      <CheckoutLink>Checkout</CheckoutLink>
+    </nav>
   </div>;
 
 export default App;
