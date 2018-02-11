cd786129cdb07b9523d05116180fda106fb202b2
sebastian correa <scorrea1@wsgc.com>
2018-02-10 21:51:28 -0500

    Add Lifecycle Hooks to a Functional Stateless Component using Recompose

--- src/App.js
+++ src/App.js
@@ -1,96 +1,45 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { withReducer, withHandlers, compose } from "recompose";
+import { withReducer, withHandlers, compose, lifecycle } from "recompose";
 import PropTypes from 'prop-types'
 
 
+const configPromise = fetchConfiguration();
 
-export const SHOW = 'SHOW';
-export const HIDE = 'HIDE';
-export const TOGGLE = 'TOGGLE';
+const withConfig = lifecycle({
+  state: { config: {} },
+  componentDidMount() {
+    configPromise.then(config =>
+      this.setState({ config }));
+  }
+});
 
-export const showAction = () => ({
-  type: SHOW,
-})
-
-export const hideAction = () => ({
-  type: HIDE,
-})
-export const toggleAction = () => ({
-  type: TOGGLE,
-})
-
-
-const StatusList = () => (
-  <div className='StatusList'>
-    <div>pending</div>
-    <div>inactive</div>
-    <div>active</div>
+const User = withConfig(({ name, status, config }) =>
+  <div className="User">
+    { name }
+    { config.showStatus && '—' + status }
+    { config.canDeleteUsers && <button>X</button> }
   </div>
 );
 
-const withToggle = compose(
-  withReducer( 'toggleOn', 'dispatch', (state, action) => {
-      switch (action.type) {
-        case SHOW:
-          return true
-        case HIDE:
-          return false
-        case TOGGLE:
-          return !state
-        default:
-          return state;
-      }
-    },
-    false),
-  withHandlers({
-    show: ({
-      dispatch /* from props */
-    }) => (e) => dispatch(showAction()),
-    hide: ({
-      dispatch /* from props */
-    }) => (e) => dispatch(hideAction()),
-    toggle: ({
-      dispatch
-    }) => (e) => dispatch(toggleAction())
-  })
-)
-
-
-const Status = withToggle(
-  ({ status,toggleOn, toggle  }) => (
-    <div onClick={toggle}>
-      {status}
-      {toggleOn && <StatusList />}
-    </div>
-  )
-);
-
-const Tooltip = withToggle(
-  ({ text, children, toggleOn, show, hide }) => (
-    <span>
-      { toggleOn && <div className='Tooltip'> {text} </div>}
-      <span onMouseEnter={show} onMouseLeave={hide}> 
-        {children} 
-      </span>
-    </span>
-  )
-)
- 
-
-
-const User = ({ name, status }) =>
-  <div className="User">
-    <Tooltip text="Cool Dude!">{ name }</Tooltip>—
-    <Status status={ status }  />
+const App = () =>
+  <div>
+    <User name="Tim" status="active" />
   </div>;
 
 
-const App = () =>(
-  <div>
-    <User name='Tim' status="active"/>
-  </div>
-)
+// Mock Configuration
+
+const config = {
+  showStatus: true,
+  canDeleteUsers: true
+}
+
+function fetchConfiguration() {
+  return new Promise((resolve) => {
+    setTimeout(() => resolve(config), 300);
+  });
+}
 
 export default App;
